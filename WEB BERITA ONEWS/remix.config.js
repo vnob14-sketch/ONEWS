0b2da6ca3fa4541e1de566ba644ae8546114affb
vnob14-sketch <vnob14@gmail.com>
2026-09-22 16:03:40 +0800

    Update remix.config.js

--- WEB BERITA ONEWS/remix.config.js
+++ WEB BERITA ONEWS/remix.config.js
@@ -3,10 +3,17 @@ export default {
   ignoredRouteFiles: ["**/.*"],
   server: "./server.js",
   serverBuildPath: "functions/[[path]].js",
-  serverConditions: ["workerd"],
+  serverConditions: ["workerd", "worker", "browser"], // Ditambahkan dukungan worker
   serverDependenciesToBundle: "all",
   serverMainFields: ["browser", "module", "main"],
   serverMinify: true,
   serverModuleFormat: "esm",
   serverPlatform: "neutral",
+  serverNodeBuiltinsPolyfill: {
+    modules: {
+      stream: true, // Memaksa injeksi polyfill otomatis untuk modul "stream"
+      crypto: true
+    }
+  }
 };
+
