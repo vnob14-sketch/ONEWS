20851c79e5912f05bdbcb6c1c4c91dd71d95cc15
vnob14-sketch <vnob14@gmail.com>
2026-09-22 15:35:50 +0800

    Add files via upload

---
+++ WEB BERITA ONEWS/remix.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  server: "./server.js",
+  serverBuildPath: "functions/[[path]].js",
+  serverConditions: ["workerd"],
+  serverDependenciesToBundle: "all",
+  serverMainFields: ["browser", "module", "main"],
+  serverMinify: true,
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
+};
